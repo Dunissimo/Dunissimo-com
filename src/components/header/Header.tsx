f30cbea51ed7eaa914cc3ad59b2b381fd8e694d5
Dunissimo <SuperDen214@yandex.ru>
2023-07-25 13:47:00 +0400

    change theme

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,12 +3,25 @@ import { FC } from "react";
 import gh from "../../assets/logo/gh.svg";
 import tg from "../../assets/logo/tg.svg";
 import vk from "../../assets/logo/vk.svg";
+import { Link } from "react-router-dom";
+
+import "./index.css";
 
 const Header: FC = () => {
   return (
-    <header className="border py-4">
+    <header className="header py-4">
       <div className="container flex justify-between items-center">
         <h1 className="text-3xl font-bold">Мои проекты</h1>
+
+        <nav className="justify-self-start flex gap-4">
+          <li className="list-none text-white hover:text-gray-400">
+            <Link to="/">Главная</Link>
+          </li>
+
+          <li className="list-none text-white hover:text-gray-400">
+            <Link to="/about">Обо мне</Link>
+          </li>
+        </nav>
 
         <ul className="flex gap-4">
           <li>
--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,13 +1,15 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 import img from "../../assets/todopng.png";
-import { Link } from "react-router-dom";
+
+import "./index.css";
 
 const Project: FC = () => {
   return (
     <Link
       to="/project/1"
-      className="lg:w-[30%] border cursor-pointer shadow hover:shadow-md"
+      className="project lg:w-[30%] bg-white text-black border cursor-pointer  hover:shadow-md"
     >
       <>
         <div className="screenshot border-b">
